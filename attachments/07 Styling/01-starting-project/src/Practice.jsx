3ec4f5c2563f6bedf8bff4da5bbaa201a86a24a9
中畠昇汰 <pandora.osk@gmail.com>
2025-01-10 06:28:40 +0800

    react-complete-guide-course-resources/attachments/07 Styling_4

--- attachments/07 Styling/01-starting-project/src/Practice.jsx
+++ attachments/07 Styling/01-starting-project/src/Practice.jsx
@@ -1,23 +1,40 @@
-import React from 'react';
+import React from "react";
 function App() {
-    const [styleState, setStyleState] = React.useState("white");
-    
-    function handleColorGreen(e) {
-        setStyleState("green")
-    }
-    function handleColorRed(e) {
-        setStyleState("red")
-    }
- 
-    return (
+  const [styleState, setStyleState] = React.useState("");
+
+  function handleStyleGreen() {
+    setStyleState("highlight-green");
+  }
+  function handleStyleRed() {
+    setStyleState("highlight-red");
+  }
+  return (
     <div id="app">
-      <h1 style={{color: {styleState}}} >CSS is great!</h1>
+      <h1
+        // className={`${
+        //   styleState === "highlight-green"
+        //   ? "highlight-green"
+        //   : styleState === "highlight-green"
+        //   ?"highlight-green"
+        //   :""
+        // }`}
+        className={`${
+          styleState === "highlight-green"
+            ? "highlight-green"
+            : styleState ==="highlight-red"
+            ? "highlight-red"
+            :""
+
+        }`}
+      >
+        CSS is great!
+      </h1>
       <menu>
         <li>
-          <button onClick={handleColorGreen} >Yes</button>
+          <button onClick={handleStyleGreen}>Yes</button>
         </li>
         <li>
-          <button onClick={handleColorRed}>No</button>
+          <button onClick={handleStyleRed}>No</button>
         </li>
       </menu>
     </div>
