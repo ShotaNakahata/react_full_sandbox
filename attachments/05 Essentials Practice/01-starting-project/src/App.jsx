eb364cbc7ddee223e3d4e6880cea76fb39bef0b7
中畠昇汰 <pandora.osk@gmail.com>
2025-01-09 17:04:42 +0800

    react-complete-guide-course-resources/attachments/05 Essentials Practice_5

--- attachments/05 Essentials Practice/01-starting-project/src/App.jsx
+++ attachments/05 Essentials Practice/01-starting-project/src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import Header  from "./components/Header";
+import Header from "./components/Header";
 import UserInput from "./components/UserInput";
 import Result from "./components/Result";
 function App() {
@@ -10,21 +10,25 @@ function App() {
     duration: 10,
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   const handleInput = (e) => {
     let { name, value } = e.target;
-    
-    setUserInput(prev=>({
-        ...prev,
-        [name]:+value
-    }))
+
+    setUserInput((prev) => ({
+      ...prev,
+      [name]: +value,
+    }));
   };
   return (
     <>
       <Header></Header>
       <UserInput userInput={userInput} handleInput={handleInput}></UserInput>
-      <Result userInput={userInput}></Result>
+      {inputIsValid && <Result userInput={userInput}></Result>}
+      {!inputIsValid && <p className="center">Prease enter duration greater then zero</p>}
     </>
   );
 }
 
 export default App;
+//Prease enter duration greater then zero
