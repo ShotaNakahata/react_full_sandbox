ae6f6b160b1452879f960cef92e8502f70dec94b
中畠昇汰 <pandora.osk@gmail.com>
2025-01-09 15:34:53 +0800

    react-complete-guide-course-resources/attachments/05 Essentials Practice_3

--- attachments/05 Essentials Practice/01-starting-project/src/components/UserInput.jsx
+++ attachments/05 Essentials Practice/01-starting-project/src/components/UserInput.jsx
@@ -1,32 +1,70 @@
 //Initial Investment _  Annual Investment _ Expect return  _ Duration
 import React from "react";
+import { useState } from "react";
 
 function UserInput() {
+  const [userInput, setUserInput] = useState({
+    InitialInvestment: 1000,
+    AnnualInvestment: 600,
+    ExpectReturn: 6,
+    Duration: 10,
+  });
+
+  const handleInput = (e) => {
+    let { name, value } = e.target;
+    setUserInput(prev=>({
+        ...prev,
+        [name]:value
+    }))
+  };
+
   return (
     <section id="user-input">
-        
       <div className="input-group">
         <label htmlFor="">
           <p>Initial Investment</p>
-          <input type="number" required/>
+          <input
+            type="number"
+            name="InitialInvestment"
+            required
+            onChange={handleInput}
+            value={userInput.InitialInvestment}
+          />
         </label>
         <label htmlFor="">
           <p>Annual Investment</p>
-          <input type="number" required/>
+          <input
+            type="number"
+            name="AnnualInvestment"
+            required
+            onChange={handleInput}
+            value={userInput.AnnualInvestment}
+          />
         </label>
       </div>
 
       <div className="input-group">
         <label htmlFor="">
           <p> Expect return</p>
-          <input type="number" required/>
+          <input
+            type="number"
+            name="ExpectReturn"
+            required
+            onChange={handleInput}
+            value={userInput.ExpectReturn}
+          />
         </label>
         <label htmlFor="">
           <p>Duration</p>
-          <input type="number" required/>
+          <input
+            type="number"
+            name="Duration"
+            required
+            onChange={handleInput}
+            value={userInput.Duration}
+          />
         </label>
       </div>
-
     </section>
   );
 }
