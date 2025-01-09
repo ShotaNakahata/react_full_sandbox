dcf132bfc0b76e5142d3c966ec248c8ff84aacb2
中畠昇汰 <pandora.osk@gmail.com>
2025-01-09 14:52:39 +0800

    react-complete-guide-course-resources/attachments/05 Essentials Practice_1

---
+++ attachments/05 Essentials Practice/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header id="header">
+      <img src="/investment-calculator-logo.png" alt="headerPhoto" />
+      <h1>Investment Calculator</h1>
+    </header>
+  );
+}
+
+export default Header;
