e8c3e10cda68f31d715302788cef39cf2928821c
中畠昇汰 <pandora.osk@gmail.com>
2025-01-10 11:30:00 +0800

    08 Refs Portals_2

--- attachments/08 Refs Portals/01-starting-project/src/components/Player.jsx
+++ attachments/08 Refs Portals/01-starting-project/src/components/Player.jsx
@@ -1,10 +1,29 @@
+import { useRef } from "react";
+import { useState } from "react";
+
 export default function Player() {
+  const playerName = useRef()
+  const [enteredPlayerName, setEnteredPlayerName] = useState("");
+  // const [submitted, setSubmitted] = useState(false);
+
+  // function handleName(e) {
+  //   setSubmitted(false)
+  //   setEnteredPlayerName(e.target.value);
+  // }
+
+  function handleSubmit() {
+    setEnteredPlayerName(playerName.current.value)
+  }
+
   return (
     <section id="player">
-      <h2>Welcome unknown entity</h2>
+      <h2>{`Welcome ${enteredPlayerName ?? "unknown"} entity`}</h2>
       <p>
-        <input type="text" />
-        <button>Set Name</button>
+        <input 
+        type="text" 
+        ref={playerName}/>
+
+        <button onClick={handleSubmit}>Set Name</button>
       </p>
     </section>
   );
