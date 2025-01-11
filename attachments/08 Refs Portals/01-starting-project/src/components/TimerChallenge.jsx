ed6b71550faa3d5e4db09080bc597f1beec6e1be
中畠昇汰 <pandora.osk@gmail.com>
2025-01-11 15:37:06 +0800

    react-complete-guide-course-resources/attachments/08 Refs Portals_10

--- attachments/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
+++ attachments/08 Refs Portals/01-starting-project/src/components/TimerChallenge.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 
 function TimerChallenge({ title, targetTime }) {
   const [timerExpired, settimerExpired] = useState(false);
   const [timerStarted, settimerStarted] = useState(false);
+  const timer = useRef()
 
+  // let timer;
   function handleTimerStart() {
     settimerStarted(true);
 
-    setTimeout(() => {
+    timer.current = setTimeout(() => {
       settimerExpired(true);
     }, targetTime * 1000);
+  }
+  function handleTimerStop() {
+    clearTimeout(timer.current);
   }
   return (
     <section className="challenge">
@@ -19,13 +24,13 @@ function TimerChallenge({ title, targetTime }) {
         {targetTime} second{targetTime > 1 ? "s" : ""}
       </p>
       <p>
-        <button onClick={handleTimerStart}>
+        <button onClick={timerStarted ? handleTimerStop : handleTimerStart}>
           {timerStarted ? "Stop" : "Start"} Challenge
         </button>
       </p>
       <p className={timerStarted ? "active" : "null"}>
-        {timerStarted?"Time Is Running":"Timer is inactive"}
-        </p>
+        {timerStarted ? "Time Is Running" : "Timer is inactive"}
+      </p>
     </section>
   );
 }
