d67144ca4171b801ceb35d1d2663ad1146cc443b
中畠昇汰 <pandora.osk@gmail.com>
2025-01-12 14:52:08 +0800

    01-starting-project_6

--- attachments/09 Practice Project - Project Management/01-starting-project/src/App.jsx
+++ attachments/09 Practice Project - Project Management/01-starting-project/src/App.jsx
@@ -1,10 +1,12 @@
 import NewProject from "./components/NewProject";
 import ProjectSidebar from "./components/ProjectsSidebar";
+import NoProjectSelected from "./components/NoProjectSelected";
 function App() {
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar />
       <NewProject />
+      <NoProjectSelected/>
     </main>
   );
 }
--- attachments/09 Practice Project - Project Management/01-starting-project/src/components/Input.jsx
+++ attachments/09 Practice Project - Project Management/01-starting-project/src/components/Input.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 
 function Input({ textarea, label, ...props }) {
-    const styledInput =" w-full p-1 border-b-2 rounded-sm border-stone-100 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
+  const styledInput =
+    " w-full p-1 border-b-2 rounded-sm border-stone-100 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
   return (
     <p className="flex flex-col gap-1 my-4">
-      <label className=" text-sm uppercase font-bold text-stone-500">{label}</label>
-      {textarea ? <textarea {...props} className={styledInput}/> : <input {...props}  className={styledInput}/>}
+      <label className=" text-sm uppercase font-bold text-stone-500">
+        {label}
+      </label>
+      {textarea ? (
+        <textarea {...props} className={styledInput} />
+      ) : (
+        <input {...props} className={styledInput} />
+      )}
     </p>
   );
 }
