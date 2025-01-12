94e6cf21b4f0444d640aeddbec495c99819faa25
中畠昇汰 <pandora.osk@gmail.com>
2025-01-12 13:02:06 +0800

    01-starting-project_3

---
+++ attachments/09 Practice Project - Project Management/01-starting-project/src/components/ProjectsSidebar.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function ProjectsSidebar() {
+  return (
+    <aside className=' w-1/3'>
+        <h2>Youe Prohject</h2>
+        <div>
+            <button>+ add Project</button>
+        </div>
+        <ul>...some project</ul>
+    </aside>
+  )
+}
+
+export default ProjectsSidebar
