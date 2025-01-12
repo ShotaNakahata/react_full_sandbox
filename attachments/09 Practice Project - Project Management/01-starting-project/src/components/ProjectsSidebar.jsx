178c7e0d2b1d59ca01340e7e2602e010726a5b56
中畠昇汰 <pandora.osk@gmail.com>
2025-01-12 13:16:10 +0800

    01-starting-project_4

--- attachments/09 Practice Project - Project Management/01-starting-project/src/components/ProjectsSidebar.jsx
+++ attachments/09 Practice Project - Project Management/01-starting-project/src/components/ProjectsSidebar.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function ProjectsSidebar() {
   return (
-    <aside className=' w-1/3'>
-        <h2>Youe Prohject</h2>
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
+        <h2 className='mb-8 uppercase font-bold md:text-xl text-stone-200'>Youe Prohject</h2>
         <div>
-            <button>+ add Project</button>
+            <butto className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100">
+              + add Project
+              </butto>
         </div>
         <ul>...some project</ul>
     </aside>
