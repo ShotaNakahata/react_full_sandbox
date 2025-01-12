141d21053a2dccad6edc263fbf224cb55e3526dc
中畠昇汰 <pandora.osk@gmail.com>
2025-01-12 12:44:27 +0800

    react-complete-guide-course-resources/attachments/08 Refs Portals_15

--- attachments/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
+++ attachments/08 Refs Portals/01-starting-project/src/components/ResultModal.jsx
@@ -1,30 +1,35 @@
 import React, { useImperativeHandle, useRef } from "react";
+import { createPortal } from "react-dom";
 import { forwardRef } from "react";
 
 const ResultModal = forwardRef(function ResultModal(
-  { targetTime, timerRemaining ,reset},
+  { targetTime, timerRemaining, reset },
   ref
 ) {
   const dialogRef = useRef();
   const result = timerRemaining <= 0 ? "lose" : "win";
   const formattedTimerRemaining = (timerRemaining / 1000).toFixed(2);
+  const score = Math.round((1 - timerRemaining / (targetTime*1000)) * 100);
 
   useImperativeHandle(ref, () => ({
     open: () => dialogRef.current.showModal(),
   }));
-  return (
+  return createPortal(
     <dialog className="result-modal" ref={dialogRef}>
-      <h2>You {result}</h2>
+      <h2>You {result}:{score}</h2>
+      
       <p>
         The target time is <strong>{targetTime} second</strong>
       </p>
       <p>
-        You stoped the timer with <strong> {formattedTimerRemaining} second left .</strong>
+        You stoped the timer with{" "}
+        <strong> {formattedTimerRemaining} second left .</strong>
       </p>
       <form method="dialog" onSubmit={reset}>
         <button>Close</button>
       </form>
-    </dialog>
+    </dialog>,
+    document.getElementById("modal")
   );
 });
 
