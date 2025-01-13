17cc81d4f85af4ce20023ceb0d33c4a3c18f6c42
中畠昇汰 <pandora.osk@gmail.com>
2025-01-13 14:21:50 +0800

    01-starting-project_10

--- attachments/09 Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
+++ attachments/09 Practice Project - Project Management/01-starting-project/src/components/NewProject.jsx
@@ -1,22 +1,39 @@
 import React, { useRef } from "react";
 import Input from "./Input";
+import Modal from "./Modal";
 
 function NewProject({ onAdd }) {
   const titleRef = useRef();
   const descriptionRef = useRef();
   const dueDateRef = useRef();
-  function handleSave(params) {
+  const modalRef = useRef()
+
+  function handleSave() {
     const enteredTitle = titleRef.current.value;
     const entereddescription = descriptionRef.current.value;
     const entereddueDate = dueDateRef.current.value;
 
-    onAdd({
-      title: enteredTitle,
-      description: entereddescription,
-      dueDate: entereddueDate,
-    });
+    //なぜ直接 !enteredTitle||!entereddescription のようにしないのですか？値が存在すればtureになりませんか？trimが必要な理由を教えて
+    if (
+      enteredTitle.trim() === "" ||
+      entereddescription.trim() === "" ||
+      entereddueDate.trim() === ""
+    ){
+        modalRef.current.open();
+        return;
+    }
+      onAdd({
+        title: enteredTitle,
+        description: entereddescription,
+        dueDate: entereddueDate,
+      });
   }
   return (
+    <>
+    <Modal ref={modalRef} buttonCaption="Close">
+        <h2>Invalid Input</h2>
+        <p>Oops... looks like you forgot to enter value</p>
+    </Modal>
     <div className="w-[35rem] mt-16 ">
       <menu className="flex items-center justify-end gap-4 my-4 bg-stone-400">
         <li>
@@ -39,6 +56,7 @@ function NewProject({ onAdd }) {
         <Input type="date" ref={dueDateRef} label="Due Date"></Input>
       </div>
     </div>
+    </>
   );
 }
 
