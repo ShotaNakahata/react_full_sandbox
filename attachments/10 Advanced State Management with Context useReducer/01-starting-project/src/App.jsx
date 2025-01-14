b9d78473256b50a5de0b95e3f7d80a969c47ccb9
中畠昇汰 <pandora.osk@gmail.com>
2025-01-14 21:28:37 +0800

    10 Advanced State Management with Context useReducer_7

--- attachments/10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
+++ attachments/10 Advanced State Management with Context useReducer/01-starting-project/src/App.jsx
@@ -1,19 +1,13 @@
-import { useState } from "react";
 import { DUMMY_PRODUCTS } from "./dummy-products.js";
 import Header from "./components/Header.jsx";
 import Shop from "./components/Shop.jsx";
 import Product from "./components/Product.jsx";
-import { CartContext } from "./store/shopping-cart-context.jsx";
+import { CartContextProvider } from "./store/shopping-cart-context.jsx";
 
 function App() {
-  
-
   return (
-    <CartContext.Provider value={ctxValue}>
-      <Header
-        // cart={shoppingCart}
-        // onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
-      />
+    <CartContextProvider>
+      <Header />
       <Shop>
         {DUMMY_PRODUCTS.map((product) => (
           <li key={product.id}>
@@ -21,7 +15,7 @@ function App() {
           </li>
         ))}
       </Shop>
-    </CartContext.Provider>
+    </CartContextProvider>
   );
 }
 
