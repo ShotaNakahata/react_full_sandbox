0119b05ddd001592aac5d9995cd8fbe8b8ee91c7
中畠昇汰 <pandora.osk@gmail.com>
2025-01-15 20:23:57 +0800

    Demo Project - React Quiz_2

--- attachments/13 Demo Project - React Quiz/01-starting-project/src/App.jsx
+++ attachments/13 Demo Project - React Quiz/01-starting-project/src/App.jsx
@@ -1,10 +1,14 @@
-import Header from "./components/Header";
 import React from "react";
+import Header from "./components/Header";
+import Quiz from "./components/Quiz";
 
 function App() {
     return (
         <>
-            <Header></Header>
+            <Header />
+            <main>
+                <Quiz />
+            </main>
         </>
     )
 }
--- attachments/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
+++ attachments/13 Demo Project - React Quiz/01-starting-project/src/components/Quiz.jsx
@@ -1,12 +1,43 @@
 import React from 'react'
 import { useState } from 'react'
+import QUESTIONS from "../question";
+import quizCompletedImg from "../assets/quiz-complete.png";
 
 function Quiz() {
-    const [activeQueationIdx,setActiveQueationIdx] = useState(0)
-    const [uerAnswers,setUerAnswers] = useState([])
-  return (
-    <p>Currently active Queastion</p>
-  )
+    const [uerAnswers, setUerAnswers] = useState([])
+    const activeQuestionIdx = uerAnswers.length;
+    function handleSelectedAnswer(selectedAnswer) {
+        setUerAnswers(prev=>[...prev,selectedAnswer])
+    }
+    const quizCompleted = activeQuestionIdx===QUESTIONS.length
+    if(quizCompleted){
+        return(
+            <div id='summary'>
+                <img src={quizCompletedImg} alt="Tropht Icon" />
+                <h2>Quiz Completed</h2>
+            </div>
+        )
+    }
+    const shuffledAnswer =[...QUESTIONS[activeQuestionIdx].answers]
+    shuffledAnswer.sort(()=>Math.random()-0.5)
+    return (
+        <div id='quiz'>
+            <div id='quwstions'>
+            <h2>{QUESTIONS[activeQuestionIdx].text}</h2>
+            <ul>
+                {shuffledAnswer.map((answer) => {
+                    return (
+                        <li key={answer} className='answer'>
+                            <button onClick={()=>handleSelectedAnswer(answer)}>
+                                {answer}
+                            </button>
+                        </li>
+                    )
+                })}
+            </ul>
+        </div>
+        </div>
+    )
 }
 
 export default Quiz
