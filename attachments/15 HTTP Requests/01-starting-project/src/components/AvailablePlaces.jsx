8a5f5301e4f1d0314ce62a012435140744682da4
中畠昇汰 <pandora.osk@gmail.com>
2025-01-18 01:25:10 +0800

    15 HTTP Requests_5

--- attachments/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ attachments/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -3,21 +3,34 @@
 import { useState } from 'react';
 import Places from './Places.jsx';
 import { useEffect } from 'react';
+import ErrorPage  from "../../../Error.jsx";
 
 export default function AvailablePlaces({ onSelectPlace }) {
   const [isFetching, setIsFetching] = useState(false)
   const [availablePlace, setAvailablePlace] = useState([])
+  const [error, setError] = useState()
 
   useEffect(() => {
     async function fetchPlaces() {
       setIsFetching(true)
-      const response = await fetch("http://localhost:3000/places")
-      const resData = await response.json()
-      setAvailablePlace(resData.places)
+      try {
+        const response = await fetch("http://localhost:3000/places")
+        const resData = await response.json()
+        if (!response.ok) {
+          throw new Error("failed to fetch places")//なぜここの条件文が必要なのか？もしfetch内のpromiseがrejectされたら自動的にcatchの方に行かないのか？それとも明示的にnew errorでerrorを生成しないとcatchがエラーがアタと認識しないのか
+        }
+        setAvailablePlace(resData.places)
+      } catch (error) {
+        setError({message:error.message||"faild to fetch place data"})
+      }
       setIsFetching(false)
     }
     fetchPlaces();
   }, [])
+
+  if(error){
+    return <ErrorPage title="An error occured" message={error.message} ></ErrorPage>
+  }
 
   return (
     <Places
