9c02b7055c46f7e75764f241c9a841beac9b54e6
中畠昇汰 <pandora.osk@gmail.com>
2025-01-18 00:34:31 +0800

    15 HTTP Requests_4

--- attachments/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
+++ attachments/15 HTTP Requests/01-starting-project/src/components/AvailablePlaces.jsx
@@ -5,21 +5,26 @@ import Places from './Places.jsx';
 import { useEffect } from 'react';
 
 export default function AvailablePlaces({ onSelectPlace }) {
+  const [isFetching, setIsFetching] = useState(false)
   const [availablePlace, setAvailablePlace] = useState([])
 
-  useEffect(()=>{
-    async function fetchPlaces () {
+  useEffect(() => {
+    async function fetchPlaces() {
+      setIsFetching(true)
       const response = await fetch("http://localhost:3000/places")
       const resData = await response.json()
       setAvailablePlace(resData.places)
+      setIsFetching(false)
     }
     fetchPlaces();
-  },[])
+  }, [])
 
   return (
     <Places
       title="Available Places"
       places={availablePlace}
+      isLoading={isFetching}
+      loadingText="fetching place data..."
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
--- attachments/15 HTTP Requests/01-starting-project/src/components/Places.jsx
+++ attachments/15 HTTP Requests/01-starting-project/src/components/Places.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/react-in-jsx-scope */
-export default function Places({ title, places, fallbackText, onSelectPlace }) {
+export default function Places({ title, places, fallbackText, onSelectPlace, isLoading, loadingText }) {
   console.log(places);
   return (
     <section className="places-category">
       <h2>{title}</h2>
-      {places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
-      {places.length > 0 && (
+      {isLoading && <p className="fallback-text">{loadingText}</p>}
+      {!isLoading && places.length === 0 && <p className="fallback-text">{fallbackText}</p>}
+      {/* !isLoading && がなければどうなるか確認する */}
+      {!isLoading && places.length > 0 && (
         <ul className="places">
           {places.map((place) => (
             <li key={place.id} className="place-item">
