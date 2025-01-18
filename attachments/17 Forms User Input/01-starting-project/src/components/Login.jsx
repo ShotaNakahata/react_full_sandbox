e12e70eac4f4fafa105ff0fec79c4335b7744fbe
中畠昇汰 <pandora.osk@gmail.com>
2025-01-18 08:31:46 +0800

    17 Forms User Input_2

--- attachments/17 Forms User Input/01-starting-project/src/components/Login.jsx
+++ attachments/17 Forms User Input/01-starting-project/src/components/Login.jsx
@@ -1,8 +1,20 @@
+import { useState } from "react";
+
 /* eslint-disable react/react-in-jsx-scope */
 export default function Login() {
+  const [enteredVal, setEnteredVal] = useState({
+    enteredEmail: "",
+    enteredPassword: ""
+  })
+  function handleInputValue(identify,event) {
+    setEnteredVal(prev=>({
+      ...prev,
+      [identify]:event.target.value
+    }))
+  }
   function handleSubmit(e) {
     e.preventDefault()
-    console.log("handleSubmit")
+    console.log("enteredVal: ", enteredVal)
   }
   return (
     <form onSubmit={handleSubmit}>
@@ -11,12 +23,24 @@ export default function Login() {
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input
+            id="email"
+            type="email"
+            name="email"
+            onChange={(event)=>handleInputValue("enteredEmail",event)}
+            value={enteredVal.enteredEmail}
+          />
         </div>
 
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input
+            id="password"
+            type="password"
+            name="password"
+            onChange={(event)=>handleInputValue("enteredPassword",event)}
+            value={enteredVal.enteredPassword}
+          />
         </div>
       </div>
 
