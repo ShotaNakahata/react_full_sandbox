9bfbe431fe3a5b38b254476704b63e38ee348d32
中畠昇汰 <pandora.osk@gmail.com>
2025-01-21 07:19:16 +0800

    18 Practice Project - Food Order_2

---
+++ attachments/18 Practice Project - Food Order/01-starting-project/src/components/Header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import logo from "../assets/logo.jpg";
+
+function Header() {
+    return (
+        <header id='main-header'>
+            <div id='title'>
+                <img src={logo} alt="logo"/>
+                <h1>REACTFOOD</h1>
+            </div>
+            <nav>
+                <button>Cart (0)</button>
+            </nav>
+        </header>
+    )
+}
+
+export default Header
