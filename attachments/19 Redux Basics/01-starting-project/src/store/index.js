63746b3dcde1dfb519c538349cbaa9bddd2a7a4a
中畠昇汰 <pandora.osk@gmail.com>
2025-01-23 13:23:35 +0800

    start redux_5

---
+++ attachments/19 Redux Basics/01-starting-project/src/store/index.js
@@ -0,0 +1,18 @@
+import { createStore } from "redux";
+
+const counterReducer = (state = { counter: 0 }, action) => {
+    switch (action.type) {
+        case "increament": {
+            return { counter: state.counter + 1 }
+        }
+        case "decreament": {
+            return { counter: state.counter - 1 }
+        }
+        default:
+            return state
+    }
+}
+
+const store = createStore(counterReducer)
+
+export default store
