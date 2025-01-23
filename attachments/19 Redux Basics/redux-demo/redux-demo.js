de82cf72918121590ee04a840b32ae43defe98cf
中畠昇汰 <pandora.osk@gmail.com>
2025-01-23 12:39:06 +0800

    start redux_2

--- attachments/19 Redux Basics/redux-demo/redux-demo.js
+++ attachments/19 Redux Basics/redux-demo/redux-demo.js
@@ -1,12 +1,22 @@
 const redux = require("redux")
 
 const counterReducer = (state = { counter: 0 }, action) => {
+    switch (action.type) {
+        case "increment": {
+            return { counter: state.counter + 1 }
+        }
+        case "decrement": {
+            return { counter: state.counter - 1 }
+        }
+        default:
+            break;
+    }
     return {
         counter: state.counter + 1
     }
 }
 const store = redux.createStore(counterReducer)
-console.log("from root",store.getState())
+console.log("from root(inital)", store.getState())
 
 const counterSubscriber = () => {
     const latestStore = store.getState()
@@ -15,3 +25,4 @@ const counterSubscriber = () => {
 
 store.subscribe(counterSubscriber)
 store.dispatch({ type: "increment" })
+store.dispatch({ type: "decrement" })
