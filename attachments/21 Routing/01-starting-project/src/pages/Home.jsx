a18e093047df64a53cbf85d18839944336450e62
中畠昇汰 <pandora.osk@gmail.com>
2025-01-28 11:15:12 +0800

    21 Routing_4

--- attachments/21 Routing/01-starting-project/src/pages/Home.jsx
+++ attachments/21 Routing/01-starting-project/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import { Link } from "react-router-dom";
 function Home() {
   return (
-    <h1>My Home Paeg</h1>
+    <>
+      <h1>My Home Page</h1>
+      <p>Go to <Link to="/products">the list of product</Link></p>
+    </>
+
   )
 }
 
