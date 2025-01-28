df995fa6169bfb7ab6725ef166fcaa46c961ee1b
中畠昇汰 <pandora.osk@gmail.com>
2025-01-28 12:49:15 +0800

    21 Routing_7

--- attachments/21 Routing/01-starting-project/src/pages/Products.jsx
+++ attachments/21 Routing/01-starting-project/src/pages/Products.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
+const PRODUCTS = [
+  { id: "p1", title: "product 1" },
+  { id: "p2", title: "product 2" },
+  { id: "p3", title: "product 3" },
+]
 
 function Products() {
   return (
-    <h1>Products pages</h1>
+    <>
+      <h1>Products pages</h1>
+      {PRODUCTS.map((product)=>{
+        return(
+          <li key={product.id}>
+            <Link to={`/products/${product.id}`}>{product.title}</Link>
+          </li>
+        )
+      })}
+    </>
   )
 }
 
