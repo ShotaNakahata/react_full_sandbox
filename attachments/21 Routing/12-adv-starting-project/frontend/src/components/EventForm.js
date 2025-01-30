4b2eeb36d6311cf8ab623351baf1b1a454fe500f
中畠昇汰 <pandora.osk@gmail.com>
2025-01-30 20:09:18 +0800

    21 Routing_advance_15

--- attachments/21 Routing/12-adv-starting-project/frontend/src/components/EventForm.js
+++ attachments/21 Routing/12-adv-starting-project/frontend/src/components/EventForm.js
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable no-unused-vars */
 /* eslint-disable react/react-in-jsx-scope */
-import { useNavigate,Form } from 'react-router-dom';
+import { useNavigate, Form, useNavigation } from 'react-router-dom';
 
 import classes from './EventForm.module.css';
 
 function EventForm({ method, event }) {
   console.log("Into EventForm")
   const navigate = useNavigate();
+  const navigation = useNavigation()
+  const isSubmitting = navigation.state === "submitting"
 
   function cancelHandler() {
     navigate('..');
@@ -21,21 +23,21 @@ function EventForm({ method, event }) {
       </p>
       <p>
         <label htmlFor="image">Image</label>
-        <input id="image" type="url" name="image"  required defaultValue={event ? event.image : "https://www.w3schools.com/html/img_girl.jpg"}  />
+        <input id="image" type="url" name="image" required defaultValue={event ? event.image : "https://www.w3schools.com/html/img_girl.jpg"} />
       </p>
       <p>
         <label htmlFor="date">Date</label>
-        <input id="date" type="date" name="date" required defaultValue={event ? event.date : new Date().toISOString().split("T")[0]}/>
+        <input id="date" type="date" name="date" required defaultValue={event ? event.date : new Date().toISOString().split("T")[0]} />
       </p>
       <p>
         <label htmlFor="description">Description</label>
-        <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : "New Ivent"}/>
+        <textarea id="description" name="description" rows="5" required defaultValue={event ? event.description : "New Ivent"} />
       </p>
       <div className={classes.actions}>
-        <button type="button" onClick={cancelHandler}>
+        <button type="button" onClick={cancelHandler} disabled={isSubmitting}>
           Cancel
         </button>
-        <button>Save</button>
+        <button disabled={isSubmitting}>{isSubmitting ? "Submitting..." : "Save"}</button>
       </div>
     </Form>
   );
