42fa6e9dcc4d9b9403d9f1ece3ef5a775803b181
中畠昇汰 <pandora.osk@gmail.com>
2025-01-31 17:50:27 +0800

    21 Routing_advance_19

--- attachments/21 Routing/12-adv-starting-project/frontend/src/components/NewsletterSignup.js
+++ attachments/21 Routing/12-adv-starting-project/frontend/src/components/NewsletterSignup.js
@@ -1,16 +1,28 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { useEffect, useRef } from 'react';
 import classes from './NewsletterSignup.module.css';
+import { useFetcher } from "react-router-dom";
 
 function NewsletterSignup() {
+  const fetcher = useFetcher()
+  const inputRef = useRef()
+  const { data, state } = fetcher;
+
+  useEffect(() => {
+    if (state === "idle" && data && data.message) {
+      window.alert(data.message)
+      inputRef.current.reset()
+    }
+  }, [data, state])
   return (
-    <form method="post" className={classes.newsletter}>
+    <fetcher.Form ref={inputRef} method="post" className={classes.newsletter} action='newsletter'>
       <input
         type="email"
         placeholder="Sign up for newsletter..."
         aria-label="Sign up for newsletter"
       />
       <button>Sign up</button>
-    </form>
+    </fetcher.Form>
   );
 }
 
