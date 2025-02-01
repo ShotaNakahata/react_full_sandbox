47f7e1a79211cdbc9cc7374baec14d22efa9a72f
中畠昇汰 <pandora.osk@gmail.com>
2025-02-01 21:19:02 +0800

    22 Authentication_3

--- attachments/22 Authentication/01-starting-project/frontend/src/components/AuthForm.js
+++ attachments/22 Authentication/01-starting-project/frontend/src/components/AuthForm.js
@@ -1,14 +1,11 @@
-import { useState } from 'react';
-import { Form } from 'react-router-dom';
+// import { useState } from 'react';
+import { Form, Link, useSearchParams } from 'react-router-dom';
 
 import classes from './AuthForm.module.css';
 
 function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
-
-  function switchAuthHandler() {
-    setIsLogin((isCurrentlyLogin) => !isCurrentlyLogin);
-  }
+  const [SerchParams] = useSearchParams()
+  const isLogin = SerchParams.get("mode") === "login"
 
   return (
     <>
@@ -23,9 +20,9 @@ function AuthForm() {
           <input id="password" type="password" name="password" required />
         </p>
         <div className={classes.actions}>
-          <button onClick={switchAuthHandler} type="button">
+          <Link to={`?mode=${isLogin ? "singup" : "login"}`}>
             {isLogin ? 'Create new user' : 'Login'}
-          </button>
+          </Link>
           <button>Save</button>
         </div>
       </Form>
