fdb76c636145a2809e21489940e5641a276c1e58
中畠昇汰 <pandora.osk@gmail.com>
2025-02-02 01:28:47 +0800

    22 Authentication_4

--- attachments/22 Authentication/01-starting-project/frontend/src/pages/Authentication.js
+++ attachments/22 Authentication/01-starting-project/frontend/src/pages/Authentication.js
@@ -5,3 +5,37 @@ function AuthenticationPage() {
 }
 
 export default AuthenticationPage;
+export async function action({ request }) {
+  const url = new URL(request.url);
+  const mode = url.searchParams.get("mode");
+
+  try {
+    // フォームデータ取得
+    const formData = await request.formData();
+    const data = Object.fromEntries(formData.entries());
+    console.log("Parsed formData:", data);
+
+    // API リクエスト
+    const response = await fetch(`http://localhost:8080/${mode}`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(data),
+    });
+
+    // レスポンスの確認
+    if (!response.ok) {
+      const errorData = await response.json();
+      console.error("Error response:", errorData);
+      throw new Response(JSON.stringify(errorData), {
+        status: response.status,
+      });
+    }
+
+    const responseData = await response.json();
+    console.log("Response data:", responseData);
+    return responseData;
+  } catch (error) {
+    console.error("Request failed:", error);
+    return { message: "Request failed", error };
+  }
+}
