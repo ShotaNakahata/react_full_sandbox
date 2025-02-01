0268cf46abb6fd101b7d2a2d4ae1c97c99f9ab8b
中畠昇汰 <pandora.osk@gmail.com>
2025-02-01 14:16:53 +0800

    21 Routing_advance_20

--- attachments/21 Routing/12-adv-starting-project/frontend/src/Pages/Events.jsx
+++ attachments/21 Routing/12-adv-starting-project/frontend/src/Pages/Events.jsx
@@ -1,36 +1,40 @@
 /* eslint-disable react/react-in-jsx-scope */
+import { Suspense } from 'react';
 import EventsList from '../components/EventsList';
-import { useLoaderData } from 'react-router-dom';
+import { useLoaderData, Await } from 'react-router-dom';
 
 function EventsPage() {
-    const events = useLoaderData()
-    console.log("From Events Page (events): ", events)
+    const { events } = useLoaderData()
+
     return (
         <>
-            {<EventsList events={events} />}
+            <Suspense fallback={<p style={{ textAlign: 'center' }}>Loading...</p>}>
+                <Await resolve={events}>
+                {(loadedEvents) => <EventsList events={loadedEvents} />}
+                </Await>
+            </Suspense>
         </>
     );
 }
 
 export default EventsPage;
 
-export async function loader() {
+export async function loadEvents() {
     const response = await fetch('http://localhost:8080/events');
     if (!response.ok) {
-        // throw json({
-        //     message: "could not fetch event"
-        // },{
-        //     status:500
-        // })
         throw new Response(JSON.stringify({
             message: "could not fetch event"
         }), {
             status: 500,
             statusText: "Internal Server Error"
-        });        
+        });
     } else {
         const resData = await response.json();
         console.log("Form App.js /events (resData): ", resData)
         return resData.events
     }
+}
+
+export async function loader() {
+    return { events: loadEvents() }
 }
