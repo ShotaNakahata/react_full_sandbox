cbfee3626fa6cb6f7f7f32fe219c977f3f87dde2
中畠昇汰 <pandora.osk@gmail.com>
2025-02-04 07:25:15 +0800

    24 React Query_12

--- attachments/24 React Query/01-starting-project/src/components/Events/EventDetails.jsx
+++ attachments/24 React Query/01-starting-project/src/components/Events/EventDetails.jsx
@@ -4,25 +4,72 @@ import { Link, Outlet, useParams, useNavigate } from 'react-router-dom';
 import Header from '../Header.jsx';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { deleteEvent, fetchEvent, queryClient } from '../../util/http.js';
-//queryKey:[events]で全てのdataを取得しているならそのキャシュを使用してそこから該当idのeventを探す方法でもいいのでは？
+import ErrorBlock from '../UI/ErrorBlock.jsx';
+
 export default function EventDetails() {
   const navigate = useNavigate()
   const { id } = useParams()
-  const { data, isPending } = useQuery({
-    queryKey: ["events", { id: id }],
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["events", id],
     queryFn: ({ signal }) => fetchEvent({ signal, id })
   });
   const { mutate } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
-      queryClient.invalidateQueries({ queryKey: ["events"] })
+      queryClient.invalidateQueries({ 
+        queryKey: ["events"] 
+      })
       navigate("/events")
     }
   })
   function handleDelete() {
-    mutate({id});
+    mutate({ id });
   }
   console.log(data)
+
+  let content;
+
+  if (isPending) {
+    content = (
+      <div id='event-details-content' className='center'>
+        <p>Loading...</p>
+      </div>
+    )
+  }
+
+  if (isError) {
+    content = (
+      <div id='event-details-content' className='center'>
+        <ErrorBlock title="fail to fetch" message={error.info?.message} />
+      </div>
+    )
+  }
+
+  if (data) {
+    content = (
+
+      <>
+        <header>
+          <h1>{data.title}</h1>
+          <nav>
+            <button onClick={handleDelete}>Delete</button>
+            <Link to="edit">Edit</Link>
+          </nav>
+        </header>
+        <div id="event-details-content">
+          <img src={`http://localhost:3000/${data.image}`} alt={data.title} />
+          <div id="event-details-info">
+            <div>
+              <p id="event-details-location">{data.location}</p>
+              <time dateTime={`Todo-DateT$Todo-Time`}>{`${data.date}-${data.time}`}</time>
+            </div>
+            <p id="event-details-description">{data.description}</p>
+          </div>
+        </div>
+      </>
+
+    )
+  }
   return (
     <>
       <Outlet />
@@ -31,28 +78,9 @@ export default function EventDetails() {
           View all Events
         </Link>
       </Header>
-      {isPending && <p>Loading...</p>}
-      {data &&
-        <article id="event-details">
-          <header>
-            <h1>{data.title}</h1>
-            <nav>
-              <button onClick={handleDelete}>Delete</button>
-              <Link to="edit">Edit</Link>
-            </nav>
-          </header>
-          <div id="event-details-content">
-            <img src={`http://localhost:3000/${data.image}`} alt="IMG" />
-            <div id="event-details-info">
-              <div>
-                <p id="event-details-location">{data.location}</p>
-                <time dateTime={`Todo-DateT$Todo-Time`}>{`${data.date}-${data.time}`}</time>
-              </div>
-              <p id="event-details-description">{data.description}</p>
-            </div>
-          </div>
-        </article>
-      }
+      <article id="event-details">
+        {content}
+      </article>
     </>
   );
 }
