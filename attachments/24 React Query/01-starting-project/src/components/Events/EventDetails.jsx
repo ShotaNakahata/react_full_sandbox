12355c959516633d8b7aba3f1fae7085fa358415
中畠昇汰 <pandora.osk@gmail.com>
2025-02-04 07:42:09 +0800

    24 React Query_13

--- attachments/24 React Query/01-starting-project/src/components/Events/EventDetails.jsx
+++ attachments/24 React Query/01-starting-project/src/components/Events/EventDetails.jsx
@@ -1,12 +1,15 @@
 /* eslint-disable react/react-in-jsx-scope */
 //[{"id":"e1","title":"Web Dev Networking Night!","description":"Meet, connect, and network with fellow budding web developers. Share your experiences and learn from others.","date":"2024-09-25","time":"18:00","location":"Innovation Lounge, New York, NY","image":"meeting-networking.jpg"}
 import { Link, Outlet, useParams, useNavigate } from 'react-router-dom';
+import { useState } from "react";
 import Header from '../Header.jsx';
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { deleteEvent, fetchEvent, queryClient } from '../../util/http.js';
 import ErrorBlock from '../UI/ErrorBlock.jsx';
+import Modal from '../UI/Modal.jsx';
 
 export default function EventDetails() {
+  const [isDeleting, setIsDeleting] = useState(false)
   const navigate = useNavigate()
   const { id } = useParams()
   const { data, isPending, isError, error } = useQuery({
@@ -16,14 +19,20 @@ export default function EventDetails() {
   const { mutate } = useMutation({
     mutationFn: deleteEvent,
     onSuccess: () => {
-      queryClient.invalidateQueries({ 
-        queryKey: ["events"] 
+      queryClient.invalidateQueries({
+        queryKey: ["events"]
       })
       navigate("/events")
     }
   })
   function handleDelete() {
     mutate({ id });
+  }
+  function handleStartDelete() {
+    setIsDeleting(true)
+  }
+  function handleStopDelete() {
+    setIsDeleting(false)
   }
   console.log(data)
 
@@ -49,10 +58,19 @@ export default function EventDetails() {
     content = (
 
       <>
+        {isDeleting &&
+          <Modal onClose={handleStopDelete}>
+            <h2>Are you sure?</h2>
+            <p>Do you really want to delete?</p>
+            <div className='form-actions'>
+              <button onClick={handleStopDelete}>Cancel</button>
+              <button onClick={handleDelete}>Delete</button>
+            </div>
+          </Modal>}
         <header>
           <h1>{data.title}</h1>
           <nav>
-            <button onClick={handleDelete}>Delete</button>
+            <button onClick={handleStartDelete}>Delete</button>
             <Link to="edit">Edit</Link>
           </nav>
         </header>
