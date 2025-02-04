f89da9a4f5a6809dda70462bd01a6081b16b1043
中畠昇汰 <pandora.osk@gmail.com>
2025-02-05 02:28:51 +0800

    25 NextJS App Router Introduction_7

--- attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/MainHeader.jsx
+++ attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/MainHeader.jsx
@@ -1,16 +1,17 @@
 import React from 'react'
 import logoImg from "@/assets/logo.png";
 import Link from 'next/link';
+import classes from "./mainHeader.module.css";
 
 function MainHeader() {
   return (
-    <header>
-      <Link href="/">
-      <img src={logoImg.src} alt="food logo" />
-      Next Level Food
+    <header className={classes.header}>
+      <Link href="/" className={classes.logo}>
+        <img src={logoImg.src} alt="food logo" />
+        Next Level Food
       </Link>
 
-      <nav>
+      <nav className={classes.nav}>
         <ul>
           <li>
             <Link href="/meals">Browse Meal</Link>
