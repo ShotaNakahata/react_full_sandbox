cdc37122ebb15952b5129ed64f5d43f71f38124c
中畠昇汰 <pandora.osk@gmail.com>
2025-02-06 04:03:25 +0800

    25 NextJS App Router Introduction_17

---
+++ attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/app/not-found.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <main className='not-found'>
+        <h1>Not Found</h1>
+        <p>We could not Found Page</p>
+    </main>
+  )
+}
+
+export default NotFound
