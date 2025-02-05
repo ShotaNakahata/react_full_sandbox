2fbe83c46d2b743c45be0284778fe3c212aba88e
中畠昇汰 <pandora.osk@gmail.com>
2025-02-06 07:30:34 +0800

    25 NextJS App Router Introduction_22

--- attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/meals/ImagePicker.jsx
+++ attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/components/meals/ImagePicker.jsx
@@ -1,19 +1,37 @@
 /* eslint-disable react/prop-types */
 "use client"
-import React, { useRef } from 'react'
+import React, { useRef, useState } from 'react'
 import classes from "./ImagePicker.module.css";
+import Image from 'next/image';
 
 function ImagePicker({ label, name }) {
     const imageInputRef = useRef()
+    const [packedImage, setPickedImage] = useState()
     function handleClick() {
         imageInputRef.current.click();
+    }
+    function handleImageChange(e) {
+        const file = e.target.files[0]
+        if (!file) return;
+        const fileReader = new FileReader()
+        fileReader.onload = () => {
+            setPickedImage(fileReader.result)
+        }
+        fileReader.readAsDataURL(file);
+
     }
     return (
         <div className={classes.picker}>
             <label htmlFor={name}>{label}</label>
             <div className={classes.controls}>
+                <div className={classes.preview}>
+                    {!packedImage && <p>No image picked</p>}
+                    {/* なぜここではpackedImage.srcではなくpackedImageだけでいいのか */}
+                    {packedImage && <Image src={packedImage} alt='user picked image' fill/>}
+                </div>
                 <input
                     className={classes.input}
+                    onChange={handleImageChange}
                     ref={imageInputRef}
                     type="file"
                     id={name}
