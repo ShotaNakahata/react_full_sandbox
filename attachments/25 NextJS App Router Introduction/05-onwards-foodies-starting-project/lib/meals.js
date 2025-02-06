44a8f0e6136ed9be3b85418b9d90956065500443
中畠昇汰 <pandora.osk@gmail.com>
2025-02-07 07:49:51 +0800

    25 NextJS App Router Introduction_27

--- attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/lib/meals.js
+++ attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/lib/meals.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import sql from "better-sqlite3";
 import fs from "fs";
 import slugify from "slugify";
@@ -7,7 +8,6 @@ const db = sql("meals.db");
 
 export async function getMeals() {
     await new Promise((resolve) => setTimeout(resolve, 2000));
-    // throw new Error("Loading meal erro")
     return db.prepare("SELECT * FROM meals").all();
 }
 
@@ -19,16 +19,11 @@ export function getMeal(slug) {
 export async function saveMeal(meal) {
     meal.slug = slugify(meal.title, { lower: true })
     meal.instuction = xss(meal.instuction)
-    //元々のimageとはどんなdataなのかが知りたい具体的には?meal.imageは？meal.image.nameは？
-    // meal.image.split(".").pop()で行っていることは？どんなdataがどんなdataに変化するのか
-    console.log("meal form saveMeal: ",meal)
-    console.log("meal.image form saveMeal: ",meal.image)
     const extension = meal.image.name.split(".").pop();
     const filleName = `${meal.slug}.${extension}`
     const streem = fs.createWriteStream(`public/images/${filleName}`)
     const bufferedImage = await meal.image.arrayBuffer();
-    //write(chunk: any,ここでchunkが必要と書いてあるがchunkとは何ですか？
-    streem.write(Buffer.from(bufferedImage), (error) => {//「streem.write()は通常のbufferを求めている」と教材内で行っていましたがどういう意味？そのせいでbufferを制作した後なぜかまたbuffer fromというよくわからないメソッドをつける必要があるのですか？
+    streem.write(Buffer.from(bufferedImage), (error) => {
         if (error) {
             throw new Error("Saving Image faild")
         }
@@ -50,9 +45,4 @@ export async function saveMeal(meal) {
     })
     
 }
-
-
-// //import fs from "fs";
-// import slugify from "slugify";
-// import xss from "xss";これら三つの機能、ライブラリは何をするため？そして今回はどのように使用している？
 
