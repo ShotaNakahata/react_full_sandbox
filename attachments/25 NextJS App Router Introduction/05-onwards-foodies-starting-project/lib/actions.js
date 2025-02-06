b7a96ad628d9e488e45adf9f3c1c933b0586ebcb
中畠昇汰 <pandora.osk@gmail.com>
2025-02-07 06:17:03 +0800

    25 NextJS App Router Introduction_26

--- attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/lib/actions.js
+++ attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/lib/actions.js
@@ -1,7 +1,20 @@
 "use server"
+import { redirect } from "next/navigation"; // ✅ 正しいインポート
 import { saveMeal } from "./meals";
+
 export async function shareMeal(formData) {
     "use server";
-    const data = Object.fromEntries(formData);
-    await saveMeal(data)
+
+    const meal = {
+        title: formData.get("title"),
+        summary: formData.get("summary"),
+        instructions: formData.get("instructions"),
+        image: formData.get("image"),
+        creator: formData.get("name"),
+        creator_email: formData.get("email")
+    };
+
+    await saveMeal(meal);
+
+    redirect("/meals"); // ✅ 正しい使い方
 }
--- attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/lib/meals.js
+++ attachments/25 NextJS App Router Introduction/05-onwards-foodies-starting-project/lib/meals.js
@@ -25,18 +25,18 @@ export async function saveMeal(meal) {
     console.log("meal.image form saveMeal: ",meal.image)
     const extension = meal.image.name.split(".").pop();
     const filleName = `${meal.slug}.${extension}`
-    const streem = fs.createWriteStream(`public/images${filleName}`)
+    const streem = fs.createWriteStream(`public/images/${filleName}`)
     const bufferedImage = await meal.image.arrayBuffer();
     //write(chunk: any,ここでchunkが必要と書いてあるがchunkとは何ですか？
     streem.write(Buffer.from(bufferedImage), (error) => {//「streem.write()は通常のbufferを求めている」と教材内で行っていましたがどういう意味？そのせいでbufferを制作した後なぜかまたbuffer fromというよくわからないメソッドをつける必要があるのですか？
         if (error) {
             throw new Error("Saving Image faild")
         }
-        meal.image = `image/${filleName}`
+        meal.image = `/images/${filleName}`
 
         db.prepare(`
             INSERT INTO meals
-            (title,summary,instruction,creator,creator_email,image,slug)
+            (title,summary,instructions,creator,creator_email,image,slug)
             VALUES(
                 @title,
                 @summary,
@@ -55,3 +55,4 @@ export async function saveMeal(meal) {
 // //import fs from "fs";
 // import slugify from "slugify";
 // import xss from "xss";これら三つの機能、ライブラリは何をするため？そして今回はどのように使用している？
+
