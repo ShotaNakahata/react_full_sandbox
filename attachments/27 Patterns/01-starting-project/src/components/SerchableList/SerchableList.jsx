d2f7dd192f9fa2fb55e50451927e1109ccaf4e97
中畠昇汰 <pandora.osk@gmail.com>
2025-02-10 04:37:37 +0800

    27 Patterns_9

--- attachments/27 Patterns/01-starting-project/src/components/SerchableList/SerchableList.jsx
+++ attachments/27 Patterns/01-starting-project/src/components/SerchableList/SerchableList.jsx
@@ -1,15 +1,24 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+import { useState } from 'react'
 
-function SerchableList({items}) {
+function SerchableList({ items }) {
+  const [searchTerm, setSearchTerm] = useState("")
+  const searchResult = items.filter((item)=>
+  JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
+  )
+  function handleChange(event) {
+    setSearchTerm(event.target.value)
+  }
   return (
     <div className='searchable-list'>
-        <input type="serch" placeholder='serch'/>
-        <ul>
-            {items.map((item,index)=>{
-                return(<li key={index}>{item.toString()}</li>)
-            })}
-        </ul>
+      <input type="serch" placeholder='serch' onChange={handleChange}/>
+      <ul>
+        {searchResult.map((item, index) => {
+          // return (<li key={index}>{item.toString()}</li>)
+          return (<li key={index}>{JSON.stringify(item)}</li>)
+        })}
+      </ul>
     </div>
   )
 }
