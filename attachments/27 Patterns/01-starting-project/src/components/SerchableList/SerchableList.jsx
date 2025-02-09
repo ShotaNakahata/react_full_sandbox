430e737e6ea5eaf4c2cae29d298f4cc3aa5c4735
中畠昇汰 <pandora.osk@gmail.com>
2025-02-10 04:57:09 +0800

    27 Patterns_12

--- attachments/27 Patterns/01-starting-project/src/components/SerchableList/SerchableList.jsx
+++ attachments/27 Patterns/01-starting-project/src/components/SerchableList/SerchableList.jsx
@@ -1,14 +1,22 @@
 /* eslint-disable react/prop-types */
 import React from 'react'
+import { useRef } from 'react'
 import { useState } from 'react'
 
 function SerchableList({ items, children, itemKeyFn }) {
   const [searchTerm, setSearchTerm] = useState("")
+  const lastChange = useRef()
   const searchResult = items.filter((item) =>
     JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase())
   )
   function handleChange(event) {
-    setSearchTerm(event.target.value)
+    if (lastChange.current) {
+      clearInterval(lastChange.current)
+    }
+    lastChange.current = setInterval(() => {
+      lastChange.current = null
+      setSearchTerm(event.target.value)
+    }, 500);
   }
   return (
     <div className='searchable-list'>
